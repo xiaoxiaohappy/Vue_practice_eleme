6533f4db2b8b516fde288a00f007fd03ebed50e1
smile_ucas <xiaoxiaotvb@126.com>
2017-07-02 12:34:42 +0800

    my first vue_project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    //如果以后有一些规则编译不过，但是这些规则我们又不需要，那么可以把这些规则写在这里并且配置成0
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
